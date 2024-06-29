f3fa498da1597ece66f6d0bf314f12f6538f8379
Jeannette Pepin <jnetpepin@gmail.com>
2024-06-28 23:03:05 -0600

    Add unit test

---
+++ message_test.go
@@ -0,0 +1,23 @@
+// Test file for message.go
+package main
+
+// https://gin-gonic.com/docs/testing/
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingRoute(t *testing.T) {
+	r := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "{\"message\":\"pong\"}", w.Body.String())
+}
